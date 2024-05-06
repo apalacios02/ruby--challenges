21ee373c9272712c2314b4914babceae68fadc36
apalacios02 <allan.palacios28@gmail.com>
2024-05-06 15:51:03 -0500

    Carl_sagan challenge 1 number 1

---
+++ carl_sagan.rb
@@ -0,0 +1,18 @@
+# Author: Allan Palacios
+
+# Define the data structures
+carl  = {
+  :toast => 'cosmos',
+  :punctuation => [',', '.', '?'],
+  :words => ['know', 'for', 'we']
+}
+
+sagan = [
+  { :are => 'are', 'A' => 'a' },
+  { 'waaaaaay' => 'way', :th3 => 'the' },
+  'itself',
+  { 2 => ['to'] }
+]
+
+# Build the sentence using data from carl and sagan
+puts "We #{sagan[0][:are]} #{sagan[1]['waaaaaay']} #{carl[:words][2]} #{sagan[3][2][0]} #{carl[:toast]} #{carl[:punctuation][2]} #{sagan[2]}"
